2ddea7f99aea344f2790ac66ff08053ad9737d9d
ykiko <486685280@qq.com>
2024-02-28 19:56:53 +0800

    1. binding overload function is supported 2. binding enum is supported 3. binding global function is supported

--- pybind.h
+++ pybind.h
@@ -2,15 +2,125 @@
 
 /**
 TODO:
-    * support function overload
-    * support enum
     * support special function
+    * support default argument
 */
 
-namespace pkpy {
-struct module_ {
+namespace pkpy::cxx_bind {
+
+template <typename T> bool is_cxx_type(VM *vm, PyObject *obj) {
+  if constexpr (std::is_same_v<T, pkpy::Tuple>) {
+    return vm->isinstance(obj, VM::tp_tuple);
+  } else if constexpr (std::is_same_v<T, pkpy::List>) {
+    return vm->isinstance(obj, VM::tp_list);
+  } else if constexpr (std::is_same_v<T, pkpy::Dict>) {
+    return vm->isinstance(obj, VM::tp_dict);
+  } else if constexpr (std::is_same_v<T, pkpy::Str>) {
+    return vm->isinstance(obj, VM::tp_str);
+  } else if constexpr (std::is_same_v<T, pkpy::Bytes>) {
+    return vm->isinstance(obj, VM::tp_bytes);
+  } else if constexpr (std::is_same_v<T, pkpy::Function>) {
+    return vm->isinstance(obj, VM::tp_function);
+  } else if constexpr (std::is_same_v<T, pkpy::DummyModule>) {
+    return vm->isinstance(obj, VM::tp_module);
+  } else if constexpr (std::is_same_v<T, pkpy::Type>) {
+    return vm->isinstance(obj, VM::tp_type);
+  } else if constexpr (std::is_same_v<T, bool>) {
+    return vm->isinstance(obj, VM::tp_bool);
+  } else if constexpr (std::is_integral_v<T>) {
+    return vm->isinstance(obj, VM::tp_int);
+  } else if constexpr (std::is_floating_point_v<T>) {
+    return vm->isinstance(obj, VM::tp_float);
+  } else {
+    return vm->_cxx_typeid_map.find(typeid(T)) != vm->_cxx_typeid_map.end();
+  }
+}
+
+template <typename... Args> inline bool check_cxx_types(VM *vm, ArgsView args) {
+  if (sizeof...(Args) != args.size()) {
+    return false;
+  }
+  std::size_t index = 0;
+  return (is_cxx_type<Args>(vm, args[index++]) && ...);
+}
+
+template <typename Ret, typename... Args, std::size_t... Is>
+inline PyObject *call_func(VM *vm, Ret (*func)(Args...), ArgsView args,
+                           std::index_sequence<Is...>) {
+  if (!check_cxx_types<Args...>(vm, args)) {
+    return nullptr;
+  } else {
+    if constexpr (std::is_void_v<Ret>) {
+      func(py_cast<Args>(vm, args[Is])...);
+      return vm->None;
+    } else {
+      return py_var(vm, func(py_cast<Args>(vm, args[Is])...));
+    }
+  }
+}
+
+template <typename Ret, typename C, typename... Args, std::size_t... Is>
+inline PyObject *call_cxx_method(VM *vm, C *self, Ret (C::*method)(Args...),
+                                 ArgsView args, std::index_sequence<Is...>) {
+  if (!check_cxx_types<Args...>(vm, args)) {
+    return nullptr;
+  } else {
+    if constexpr (std::is_void_v<Ret>) {
+      (self->*method)(py_cast<Args>(vm, args[Is])...);
+      return vm->None;
+    } else {
+      return py_var(vm, (self->*method)(py_cast<Args>(vm, args[Is])...));
+    }
+  }
+}
+
+class module_ {
+  template <typename T> friend class class_;
+
+  template <typename T> friend class enum_;
+
+private:
   VM *vm;
   PyObject *mod;
+
+  using CXX_Func = std::function<PyObject *(VM *, ArgsView)>;
+  inline static std::map<pkpy::StrName, std::vector<CXX_Func>>
+      func_overload_set;
+
+public:
+  template <typename Ret, typename... Args, typename... Extra>
+  module_ &def(const char *name, Ret (*fn)(Args...), const Extra &...extras) {
+    Str name_index = name;
+    if (func_overload_set.find(name_index) == func_overload_set.end()) {
+      func_overload_set[name_index] = {};
+      auto fp = [name_index](VM *vm, ArgsView args) -> PyObject * {
+        for (auto fp : func_overload_set[name_index]) {
+          PyObject *ret = fp(vm, args);
+          if (ret) {
+            return ret;
+          }
+        }
+        vm->TypeError("no matching method found");
+        return nullptr;
+      };
+      std::string sig = name;
+      sig += "(*args)";
+      vm->bind(mod, sig.c_str(), fp);
+    }
+
+    auto fp = [fn](VM *vm, ArgsView args) -> PyObject * {
+      Tuple &pack = CAST(Tuple &, args[0]);
+      args = ArgsView(pack.begin(), pack.end());
+      return call_func(vm, fn, args,
+                       std::make_index_sequence<sizeof...(Args)>{});
+    };
+
+    func_overload_set[name_index].push_back(fp);
+    return *this;
+  }
+
+public:
+  module_(VM *vm, PyObject *mod) : vm(vm), mod(mod) {}
 };
 
 template <typename... Args> class init {
@@ -23,6 +133,13 @@ template <typename T> class class_ {
   PyObject *mod;
   PyObject *cls;
 
+  using Method = PyObject *(*)(VM *, ArgsView);
+  inline static std::vector<Method> constructor_overload_set;
+
+  using CXX_Method = std::function<PyObject *(VM *, ArgsView)>;
+  inline static std::map<pkpy::StrName, std::vector<CXX_Method>>
+      method_overload_set;
+
 private:
   template <typename... Args, std::size_t... Is>
   static PyObject *gcnew(VM *vm, Type type, PyObject **args,
@@ -34,6 +151,7 @@ public:
   class_(module_ mod_, const char *name) : vm(mod_.vm), mod(mod_.mod) {
     cls = vm->new_type_object(mod, name, 0);
     mod->attr().set(name, cls);
+    vm->_cxx_typeid_map[typeid(T)] = PK_OBJ_GET(Type, cls);
   }
 
   template <typename M> class_ &def_readwrite(const char *name, M T::*member) {
@@ -53,13 +171,33 @@ public:
   }
 
   template <typename... Args> class_ &def(init<Args...>) {
+    if (constructor_overload_set.empty()) {
+      auto fp = +[](VM *vm, ArgsView args) -> PyObject * {
+        for (auto fp : constructor_overload_set) {
+          PyObject *ret = fp(vm, args);
+          if (ret) {
+            return ret;
+          }
+        }
+        vm->TypeError("no matching constructor found");
+        return nullptr;
+      };
+      vm->bind(cls, "__new__(cls, *args)", fp);
+    }
+
     auto fp = +[](VM *vm, ArgsView args) -> PyObject * {
       Type type = PK_OBJ_GET(Type, args[0]);
       Tuple &pack = CAST(Tuple &, args[1]);
-      return gcnew<Args...>(vm, type, pack.begin(),
-                            std::index_sequence_for<Args...>{});
+      args = ArgsView(pack.begin(), pack.end());
+      if (!check_cxx_types<Args...>(vm, args)) {
+        return nullptr;
+      } else {
+        return gcnew<Args...>(vm, type, pack.begin(),
+                              std::index_sequence_for<Args...>{});
+      }
     };
-    vm->bind(cls, "__new__(cls, *args)", fp);
+
+    constructor_overload_set.push_back(fp);
     return *this;
   }
 
@@ -67,30 +205,55 @@ public:
   template <typename Ret, typename... Args, typename... Extras>
   class_ &def(const char *name, Ret (T::*member_fn)(Args...),
               const Extras &...extras) {
+    Str name_index = name;
+    if (method_overload_set.find(name_index) == method_overload_set.end()) {
+      method_overload_set[name_index] = {};
+      auto fp = [name_index](VM *vm, ArgsView args) -> PyObject * {
+        for (auto fp : method_overload_set[name_index]) {
+          PyObject *ret = fp(vm, args);
+          if (ret) {
+            return ret;
+          }
+        }
+        vm->TypeError("no matching method found");
+        return nullptr;
+      };
+      std::string sig = name;
+      sig += "(self,*args)";
+      vm->bind(cls, sig.c_str(), fp);
+    }
+
     auto fp = [member_fn](VM *vm, ArgsView args) -> PyObject * {
       T &self = PK_OBJ_GET(T, args[0]);
-      std::size_t Is = 0;
-      if constexpr (std::is_void_v<Ret>) {
-        (self.*member_fn)(py_cast<Args>(vm, args[Is += 1])...);
-        return vm->None;
-      } else {
-        return py_var(vm,
-                      (self.*member_fn)(py_cast<Args>(vm, args[Is += 1])...));
-      }
+      Tuple &pack = CAST(Tuple &, args[1]);
+      args = ArgsView(pack.begin(), pack.end());
+      return call_cxx_method(vm, &self, member_fn, args,
+                             std::make_index_sequence<sizeof...(Args)>{});
     };
 
-    std::string sig;
-    sig.reserve(sizeof...(Args) * 4);
-    sig += name;
-    sig += "(self";
-    for (std::size_t index = 0; index < sizeof...(Args); ++index) {
-      sig += ",v";
-      sig += std::to_string(index);
-    }
-    sig += ")";
-    vm->bind(cls, sig.c_str(), fp);
+    method_overload_set[name_index].push_back(fp);
     return *this;
   }
 };
 
-}; // namespace pkpy
+template <typename T> class enum_ {
+  VM *vm;
+  PyObject *mod;
+  PyObject *cls;
+  const char *cls_name;
+
+public:
+  enum_(module_ mod_, const char *name)
+      : vm(mod_.vm), mod(mod_.mod), cls_name(name) {
+    cls = vm->new_type_object(mod, name, 0);
+    mod->attr().set(name, cls);
+    vm->_cxx_typeid_map[typeid(T)] = PK_OBJ_GET(Type, cls);
+  }
+
+  enum_ &value(const char *name, T value) {
+    cls->attr().set(name,
+                    py_var(vm, static_cast<std::underlying_type_t<T>>(value)));
+    return *this;
+  }
+};
+}; // namespace pkpy::cxx_bind
--- test.cpp
+++ test.cpp
@@ -1,34 +1,116 @@
 #include "pybind.h"
 
 using namespace pkpy;
+using namespace pkpy::cxx_bind;
 
 struct Point {
   int x;
   int y;
 
+  Point(int x) : x(x), y(100) {}
+
   Point(int x, int y) : x(x), y(y) {}
 
   int sum(int z) { return x + y + z; }
+
+  int sum(int a, int b) { return x + y + a + b; }
+
+  int sub() { return x - y; }
+
+  int sub(int z) { return x + y - z; }
 };
 
-int main() {
-  // Create a virtual machine
-  VM *vm = new VM();
+void register_point(module_ &m) {
+  class_<Point>(m, "Point")
+      .def(init<int>())
+      .def(init<int, int>())
+      .def_readwrite("x", &Point::x)
+      .def_readwrite("y", &Point::y)
+      .def("sum", static_cast<int (Point::*)(int)>(&Point::sum))
+      .def("sum", static_cast<int (Point::*)(int, int)>(&Point::sum))
+      .def("sub", static_cast<int (Point::*)()>(&Point::sub))
+      .def("sub", static_cast<int (Point::*)(int)>(&Point::sub));
+}
 
-  // clang-format off
-    module_ m{vm, vm->_main};
-    class_<Point>(m, "Point")
-        .def(init<int, int>())
-        .def_readwrite("x", &Point::x)
-        .def_readwrite("y", &Point::y)
-        .def("sum", &Point::sum);
-  // clang-format on
+void test_for_point() {
+  std::cout << "-----------------------------------" << std::endl;
+  VM *vm = new VM();
+  module_ m{vm, vm->_main};
+  register_point(m);
 
   vm->exec("a = Point(1, 2)");
-  vm->exec("print(a.x)");      // 1
-  vm->exec("print(a.y)");      // 2
-  vm->exec("print(a.sum(3))"); // 6
+  vm->exec("print(a.x)");         // 1
+  vm->exec("print(a.y)");         // 2
+  vm->exec("print(a.sum(3))");    // 6
+  vm->exec("print(a.sum(3, 4))"); // 10
+  vm->exec("print(a.sub())");     // -1
+  vm->exec("print(a.sub(3))");    // 0
+
+  vm->exec("a = Point(1)");
+  vm->exec("print(a.x)");         // 1
+  vm->exec("print(a.y)");         // 100
+  vm->exec("print(a.sum(3))");    // 104
+  vm->exec("print(a.sum(3, 4))"); // 108
+  vm->exec("print(a.sub())");     // -99
+  vm->exec("print(a.sub(3))");    // 98
 
   delete vm;
+}
+
+enum class Color {
+  RED,
+  GREEN,
+  BLUE,
+  YELLOW
+
+};
+
+void register_enum(module_ &m) {
+  enum_<Color>(m, "Color")
+      .value("RED", Color::RED)
+      .value("GREEN", Color::GREEN)
+      .value("BLUE", Color::BLUE)
+      .value("YELLOW", Color::YELLOW);
+}
+
+void test_for_enum() {
+  std::cout << "-----------------------------------" << std::endl;
+  VM *vm = new VM();
+  module_ m{vm, vm->_main};
+  register_enum(m);
+
+  vm->exec("print(Color.RED)");    // 0
+  vm->exec("print(Color.GREEN)");  // 1
+  vm->exec("print(Color.BLUE)");   // 2
+  vm->exec("print(Color.YELLOW)"); // 3
+
+  delete vm;
+}
+
+int add(int x, int y) { return x + y; }
+
+int add(int x, int y, int z) { return x + y + z; }
+
+void register_global(module_ &m) {
+  m.def("add", static_cast<int (*)(int, int)>(add));
+  m.def("add", static_cast<int (*)(int, int, int)>(add));
+}
+
+void test_for_global() {
+  std::cout << "-----------------------------------" << std::endl;
+  VM *vm = new VM();
+  module_ m{vm, vm->_main};
+  register_global(m);
+
+  vm->exec("print(add(1, 2))");    // 3
+  vm->exec("print(add(1, 2, 3))"); // 6
+
+  delete vm;
+}
+
+int main() {
+  test_for_point();
+  test_for_enum();
+  test_for_global();
   return 0;
 }
