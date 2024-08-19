992644a9d395e1cf026af39b39fe85eb4d11387b
ykiko <ykikoykikoykiko@gmail.com>
2024-08-19 23:48:50 +0800

    some clear.

--- pybind11/include/pybind11/internal/type_traits.h
+++ pybind11/include/pybind11/internal/type_traits.h
@@ -1,13 +1,86 @@
 #pragma once
+
 #include <tuple>
 #include <memory>
 #include <string_view>
 #include <type_traits>
 
-namespace pybind11 {
+namespace pkbind {
+
+template <typename T>
+struct type_identity {
+    using type = T;
+};
 
 template <typename T>
 constexpr bool dependent_false = false;
+
+template <typename T>
+using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+/// check if T is one of Ts...
+template <typename T, typename... Ts>
+constexpr inline bool is_one_of = (std::is_same_v<T, Ts> || ...);
+
+using std::is_member_function_pointer_v;
+using std::is_member_object_pointer_v;
+
+/// check if T is a function pointer type
+template <typename T>
+constexpr inline bool is_function_pointer_v = std::is_function_v<std::remove_pointer_t<T>>;
+
+/// check if T is a functor type(has a unique operator())
+template <typename T, typename U = void>
+constexpr bool is_functor_v = false;
+
+template <typename T>
+constexpr inline bool is_functor_v<T, std::void_t<decltype(&T::operator())>> = true;
+
+template <typename T>
+constexpr inline bool is_integer_v = std::is_integral_v<T> && !is_one_of<T, bool, char, wchar_t>;
+
+template <typename T>
+constexpr inline bool is_floating_point_v = std::is_floating_point_v<T>;
+
+template <typename T>
+constexpr inline bool is_unique_pointer_v = false;
+
+template <typename T>
+constexpr inline bool is_unique_pointer_v<std::unique_ptr<T>> = true;
+
+template <typename T>
+constexpr bool is_pointer_v = std::is_pointer_v<T> && !std::is_same_v<T, const char*>;
+
+template <typename T>
+constexpr inline bool is_multiple_pointer_v = std::is_pointer_v<T> && is_multiple_pointer_v<std::remove_pointer_t<T>>;
+
+template <typename T>
+constexpr auto type_name() {
+#if __GNUC__ || __clang__
+    std::string_view name = __PRETTY_FUNCTION__;
+    // format is "auto type_name() [T = int]"
+    std::size_t start = name.find("= ") + 2;
+    std::size_t end = name.rfind(']');
+    return name.substr(start, end - start);
+#elif _MSC_VER
+    std::string_view name = __FUNCSIG__;
+    // format is possible one of three following:
+    // - "auto __cdecl type_name<int>(void)"
+    // - "auto __cdecl type_name<struct X>(void)"
+    // - "auto __cdecl type_name<class X>(void)"
+    std::size_t start = name.find('<') + 1;
+    std::size_t end = name.rfind(">(");
+    name = name.substr(start, end - start);
+    auto space = name.find(' ');
+    return space == std::string_view::npos ? name : name.substr(space + 1);
+#else
+    static_assert(false, "current compiler is not supported");
+#endif
+}
+
+static_assert(type_name<int>() == "int" && type_name<double>() == "double" &&
+                  type_name<std::string_view>() == "std::basic_string_view<char>",
+              "type_name() test failed, please report this issue");
 
 template <typename T, typename Tuple>
 struct tuple_push_front;
@@ -72,17 +145,6 @@ using class_type_t = typename member_traits<T>::class_type;
 
 // some traits for distinguishing between function pointers, member function pointers and
 // functors
-using std::is_member_function_pointer_v;
-using std::is_member_object_pointer_v;
-
-template <typename T>
-constexpr inline bool is_function_pointer_v = std::is_function_v<std::remove_pointer_t<T>>;
-
-template <typename T, typename U = void>
-constexpr bool is_functor_v = false;
-
-template <typename T>
-constexpr inline bool is_functor_v<T, std::void_t<decltype(&T::operator())>> = true;
 
 template <typename T, typename SFINAE = void>
 struct callable_traits;
@@ -114,59 +176,36 @@ using callable_return_t = typename callable_traits<Callable>::return_type;
 template <typename Callable>
 constexpr std::size_t callable_args_count_v = std::tuple_size_v<callable_args_t<Callable>>;
 
-template <typename T>
-struct type_identity {
-    using type = T;
+template <typename... Ts>
+struct type_list {
+    constexpr inline static int size = sizeof...(Ts);
+
+    template <typename T>
+    constexpr inline static int count = [] {
+        int count = 0;
+        ((count += std::is_same_v<T, Ts>), ...);
+        return count;
+    }();
+
+    /// find first index of T in type_list.
+    template <typename T>
+    constexpr inline static int find = [] {
+        bool arr[size + 1] = {std::is_same_v<T, Ts>...};
+        for(int i = 0; i < size; ++i) {
+            if(arr[i]) return i;
+        }
+        return -1;
+    }();
+
+    template <typename T>
+    constexpr inline static int find_last = [] {
+        bool arr[size + 1] = {std::is_same_v<T, Ts>...};
+        for(int i = size - 1; i >= 0; --i) {
+            if(arr[i]) return i;
+        }
+        return -1;
+    }();
 };
-
-template <typename T>
-using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
-
-template <typename T, typename... Ts>
-constexpr inline std::size_t types_count_v = (std::is_same_v<T, Ts> + ...);
-
-template <typename T>
-constexpr inline std::size_t types_count_v<T> = 0;
-
-template <typename T, typename... Ts>
-constexpr inline int type_index_v = [] {
-    bool arr[sizeof...(Ts) + 1] = {std::is_same_v<T, Ts>...};
-    for(int i = 0; i < sizeof...(Ts); ++i) {
-        if(arr[i]) return i;
-    }
-    return -1;
-}();
-
-static_assert(types_count_v<int, int, float, int> == 2);
-static_assert(types_count_v<int, float, double> == 0);
-static_assert(type_index_v<int, int, float, int> == 0);
-static_assert(type_index_v<float, int, float, int> == 1);
-static_assert(type_index_v<int, float, double> == -1);
-
-template <typename T>
-constexpr inline bool is_multiple_pointer_v = std::is_pointer_v<T> && is_multiple_pointer_v<std::remove_pointer_t<T>>;
-
-template <typename T>
-using pybind11_decay_t = std::decay_t<std::remove_pointer_t<std::decay_t<T>>>;
-
-template <typename T>
-constexpr auto type_name() {
-#if __GNUC__ || __clang__
-    std::string_view name = __PRETTY_FUNCTION__;
-    std::size_t start = name.find('=') + 2;
-    std::size_t end = name.size() - 1;
-    return std::string_view{name.data() + start, end - start};
-#elif _MSC_VER
-    std::string_view name = __FUNCSIG__;
-    std::size_t start = name.find('<') + 1;
-    std::size_t end = name.rfind(">(");
-    name = std::string_view{name.data() + start, end - start};
-    start = name.find(' ');
-    return start == std::string_view::npos ? name : std::string_view{name.data() + start + 1, name.size() - start - 1};
-#else
-    static_assert(false, "Unsupported compiler");
-#endif
-}
 
 template <typename... Args>
 struct overload_cast_t {
@@ -176,12 +215,12 @@ struct overload_cast_t {
     }
 
     template <typename Return, typename Class>
-    constexpr auto operator() (Return (Class::*pmf)(Args...), std::false_type = {}) const noexcept -> decltype(pmf) {
+    constexpr auto operator() (Return (Class::* pmf)(Args...), std::false_type = {}) const noexcept -> decltype(pmf) {
         return pmf;
     }
 
     template <typename Return, typename Class>
-    constexpr auto operator() (Return (Class::*pmf)(Args...) const, std::true_type) const noexcept -> decltype(pmf) {
+    constexpr auto operator() (Return (Class::* pmf)(Args...) const, std::true_type) const noexcept -> decltype(pmf) {
         return pmf;
     }
 };
@@ -195,15 +234,6 @@ constexpr inline overload_cast_t<Args...> overload_cast;
 /// Const member function selector for overload_cast
 ///  - regular: static_cast<Return (Class::*)(Arg) const>(&Class::func)
 ///  - sweet:   overload_cast<Arg>(&Class::func, const_)
-constexpr static auto const_ = std::true_type{};
+constexpr inline auto const_ = std::true_type{};
 
-template <typename T>
-constexpr inline bool is_unique_pointer_v = false;
-
-template <typename T>
-constexpr inline bool is_unique_pointer_v<std::unique_ptr<T>> = true;
-
-template <typename T>
-constexpr bool is_pointer_v = std::is_pointer_v<T> && !std::is_same_v<T, const char*>;
-
-}  // namespace pybind11
+}  // namespace pkbind
