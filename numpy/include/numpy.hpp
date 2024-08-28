ccbf52fa0a285bf6e66a29b1221e659f93e13c28
Anurag Bhat <90216905+faze-geek@users.noreply.github.com>
2024-08-28 15:16:42 +0530

    Add #pragma GCC system_header

--- numpy/include/numpy.hpp
+++ numpy/include/numpy.hpp
@@ -20,6 +20,7 @@
         #pragma GCC diagnostic push
         #pragma GCC diagnostic ignored "-Wall"
         #pragma GCC diagnostic ignored "-Wextra"
+        #pragma GCC system_header
     #endif
 #endif
 
